768f43530735dcc0eae37b7474dfa2a59fbf2acd
FranzSuarez <31463294+FranzSuarez@users.noreply.github.com>
2020-09-06 17:39:55 -0500

    Fixing errors

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from response import templateError,templateSuccess
 import mongoDB as db
 import json
-
 
 app = Flask(__name__)
 
 #definig routes
 @app.route('/users', methods = ['GET'])
 def listUsers():
-    response = db.getUsers()
-    #print(json.loads(response))
-    return f'{response}'
+    result = db.getUsers()
+    response = {
+        "Number of elements" : result[0],
+        "Recovered items" : result[1]
+    }
+    return templateSuccess(response)
+    #return Response(response, mimetype='application/json')
 
 @app.route('/users', methods = ['POST'])
 def createUser():
@@ -22,29 +25,29 @@ def createUser():
     if(number > 0):
         for element in array:
             try:
-                username = element["username"]
-                password = element["password"]
-                email = element["email"]
-
-                if username and email and password:
-                    params = {
-                        "username" : username,
-                        "password": password,
-                        "email": email
-                    }
-                    result = db.insertUser(params)
-                    response.append(result)
-                else:
-                    return templateError({"code": 400, "message": "The parameters cannot be empty"})
-
+                username = element['username']
+                password = element['password']
+                email = element['email']
             except:
-                return templateError({"code": 400, "message": "There's one or more missing parameters: username, password or email"})
+                return templateError({'code': 400, 'message': 'There is one or more missing parameters: username, password or email'})
+            
+            if username and email and password:
+                params = {
+                    'username' : username,
+                    'password': password,
+                    'email': email
+                }
+                result = db.insertUser(params)
+                response.append(result)
+
+            else:
+                return templateError({'code': 400, 'message': 'The parameters cannot be empty'})
 
     else:
-        return templateError({"code": 204, "message": "The request does not contain elements"})
+        return templateError({'code': 204, 'message': 'The request does not contain elements'})
     
     return templateSuccess(response)
+    #return templateSuccess('Messages received')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- response.py
+++ response.py
@@ -1,26 +1,26 @@
 import json
 
 def generateResponse(options):
-    success = options["success"]
-    data = options["data"]
-    error = options["error"]
+    success = options['success']
+    data = options['data']
+    error = options['error']
     result = {
-        "success": success,
-        "data": data,
-        "error": error
+        'success': success,
+        'data': data,
+        'error': error
     }
     return json.dumps(result)
 
 def templateSuccess(data):
-    result = generateResponse({"success":True, "data": data, "error": None})
+    result = generateResponse({'success':True, 'data': data, 'error': None})
     return result
     
 def templateError(options):
-    code = options["code"]
-    message = options["message"]
+    code = options['code']
+    message = options['message']
     error = {
-        "code": code,
-        "message": message
+        'code': code,
+        'message': message
     }
-    result = generateResponse({"success": False, "data": None, "error": error})
+    result = generateResponse({'success': False, 'data': None, 'error': error})
     return result
